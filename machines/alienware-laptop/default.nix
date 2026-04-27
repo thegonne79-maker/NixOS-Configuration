7af90a17ed5c12fad0d67df5f97f4db000d6a35c
John Bargman <john.bargman@platonic.systems>
2026-04-27 14:55:20 -0600

    fix: Simplify laptop config for 64GB RAM
    Removed heavy services:
    - ollama, litellm (AI services)
    - docker, libvirtd, vmware (virtualization)
    - rclone, guix, printing
    - Multiple auto-start apps
    
    Reduced to 2 build jobs to avoid OOM.

--- machines/alienware-laptop/default.nix
+++ machines/alienware-laptop/default.nix
@@ -1,56 +1,30 @@
 # Alienware 18 Area 51 Laptop Configuration
-# RTX 5090 GPU, Intel CPU - Full LINDA feature parity
+# RTX 5090 GPU, Intel CPU - Lightweight for 64GB RAM
 { config, pkgs, self, lib, hostname, ... }:
 {
   imports = [
     ./hardware-configuration.nix
-    ../../services/ollama.nix
-    ../../services/litellm.nix
     ../../modules/enable-wg.nix
-    ../../lib/rclone-target.nix
     ../../environments/i3wm_darthpjb.nix
     ../../environments/steam.nix
     ../../environments/code.nix
     ../../environments/neovim.nix
     ../../environments/communications.nix
-    ../../environments/emacs.nix
     ../../environments/browsers.nix
-    ../../environments/mudd.nix
-    ../../environments/cad_and_graphics.nix
-    ../../environments/3dPrinting.nix
-    ../../environments/audio_visual_editing.nix
     ../../environments/general_fonts.nix
-    ../../environments/video_call_streaming.nix
-    ../../environments/cloud_and_backup.nix
     ../../locale/tailscale.nix
-    ../../environments/rtl-sdr.nix
     ../../modifier_imports/bluetooth.nix
-    ../../modifier_imports/memtest.nix
     ../../modifier_imports/hosts.nix
     ../../modifier_imports/zfs.nix
-    ../../modifier_imports/virtualisation-libvirtd.nix
-    ../../modifier_imports/virtualisation-vmware.nix
     ../../environments/sshd.nix
     ../../modifier_imports/cuda.nix
-    ../../modifier_imports/remote-builder.nix
     ../../users/darthpjb.nix
   ];
 
   environment = {
     vpn = {
       enable = true;
-      postfix = 50;  # Unique IP: 10.88.127.50 for Tailscale
-    };
-    rclone-target = {
-      enable = true;
-      configFile = "${self}/secrets/rclone-config-file";
-      targets = {
-        obsidian-v3 = {
-          filePath = " /bulk-storage/50-DB-v3/";
-          remoteName = "minio:obsidian-v3";
-          syncInterval = 60; # every minute
-        };
-      };
+      postfix = 50;
     };
   };
 
@@ -59,17 +33,8 @@
     hostId = "1b7840e4";
     useDHCP = lib.mkDefault true;
     firewall = {
-      allowedTCPPorts = [ 22 1108 47984 47989 47990 48010 4010 27015 4549 24070 ];
-      allowedUDPPorts = [ 2108 2107 4010 27015 4175 4179 4171 47998 47999 48000 48002 ];
-      allowedTCPPortRanges = [
-        { from = 17780; to = 17785; }
-        { from = 47984; to = 48010; }
-      ];
-      allowedUDPPortRanges = [
-        { from = 17780; to = 17785; }
-        { from = 27031; to = 27036; }
-        { from = 47984; to = 48010; }
-      ];
+      allowedTCPPorts = [ 22 1108 ];
+      allowedUDPPorts = [ 2108 ];
     };
   };
 
@@ -99,7 +64,7 @@
     };
   };
 
-  # Sunshine for streaming (Moonlight compatible)
+  # Sunshine for streaming
   services.sunshine = {
     enable = true;
     autoStart = true;
@@ -107,139 +72,35 @@
     capSysAdmin = true;
   };
 
-  # Docker support
-  virtualisation.docker.enable = true;
-
-  # Laptop-specific packages
+  # Laptop packages - minimal
   environment.systemPackages = with pkgs; [
-    # Power management
     powertop
     acpi
-
-    # Remote access
-    rustdesk
-
-    # Gaming
-    steam
-    lutris
-
-    # VM and passthrough
-    self.inputs.nixpkgs_unstable.legacyPackages.x86_64-linux.looking-glass-client
-    self.inputs.nixpkgs_unstable.legacyPackages.x86_64-linux.scream
-    virtiofsd
-    gwe
-    virt-manager
+    vulkan-tools
   ];
 
-  # Nix build settings for laptop - reduced for memory
+  # Nix build settings - very conservative
   nix = {
     settings = {
-      max-jobs = lib.mkForce 4;
+      max-jobs = lib.mkForce 2;
       cores = lib.mkDefault 0;
-      download-buffer-size = 524288000;
     };
-    nrBuildUsers = 4;
+    nrBuildUsers = 2;
   };
 
-  # Audio configuration with Discord/Vivaldi fixes
+  # Audio
   services.pipewire = {
-    extraConfig.pipewire-pulse = {
-      "50-discord-block-source-volume" = {
-        "pulse.rules" = [
-          {
-            matches = [
-              { application.process.binary = "Discord"; }
-              { application.process.binary = ".Discord-wrapped"; }
-              { application.process.binary = "discord"; }
-              { application.process.binary = "*[Dd]iscord*"; }
-            ];
-            actions = { quirks = [ "block-source-volume" ]; };
-          }
-        ];
-      };
-      "50-vivaldi-block-source-volume" = {
-        "pulse.rules" = [
-          {
-            matches = [
-              { application.process.binary = "*[V]ivaldi*"; }
-            ];
-            actions = { quirks = [ "block-source-volume" ]; };
-          }
-        ];
-      };
-    };
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
 
-  # System services
-  services.printing.enable = true;
-  services.guix.enable = true;
-  programs.adb.enable = true;
-  users.users.John88.extraGroups = [ "adbusers" ];
-
-  # Auto-start user services
-  systemd.user.services = {
-    obsidian = {
-      description = "obsidian-autostart";
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig = {
-        Restart = "always";
-        ExecStart = ''
-          ${pkgs.obsidian}/bin/obsidian
-        '';
-        PassEnvironment = "DISPLAY XAUTHORITY";
-      };
-    };
-    dino = {
-      description = "dino-autostart";
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig = {
-        Restart = "always";
-        ExecStart = ''
-          ${pkgs.dino}/bin/dino
-        '';
-        PassEnvironment = "DISPLAY XAUTHORITY";
-      };
-    };
-    discord = {
-      description = "discord-autostart";
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig = {
-        Restart = "always";
-        ExecStart = ''
-          ${pkgs.discord}/bin/discord
-        '';
-        PassEnvironment = "DISPLAY XAUTHORITY";
-      };
-    };
-    scream-ivshmem = {
-      enable = true;
-      description = "Scream br0";
-      serviceConfig = {
-        ExecStart = "${pkgs.scream}/bin/scream -u -i wlan0 -p 4010";
-      };
-      wantedBy = [ "multi-user.target" ];
-      requires = [ "pipewire.service" ];
-    };
-  };
-
-  # Tmpfiles for Looking Glass
-  systemd.tmpfiles.rules = [
-    "f /dev/shm/looking-glass 0660 John88 qemu-libvirtd -"
-    "d /rendercache 0755 John88 users"
-  ];
-
   # Timezone
   time.timeZone = "Etc/UTC";
 
   # Boot configuration
   boot = {
-    tmp.useTmpfs = false;
-    supportedFilesystems = [ "zfs" "ntfs" ];
-    zfs.extraPools = [ "speed-storage" "bulk-storage" ];
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
